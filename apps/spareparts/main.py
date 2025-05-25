c9f0fd900505817b5963c3f1547fddb40b0c3688
Soheil <hesarisoheil1999@example.com>
2025-05-25 11:09:23 +0330

    [FIX]-4

--- apps/spareparts/main.py
+++ apps/spareparts/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from apps.spareparts.api.facory_parts_api import factory_parts_router
 # from apps.spareparts.api.spareparts import sparePartsRouter
 from apps.spareparts.api.uint_of_measue_api import uom_router
 from dotenv import load_dotenv
@@ -9,3 +10,4 @@ from dotenv import load_dotenv
 app = FastAPI()
 # app.include_router(sparePartsRouter)
 app.include_router(uom_router)
+app.include_router(factory_parts_router)
