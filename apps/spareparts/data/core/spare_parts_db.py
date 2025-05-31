ff36bddd05ca7d8954ff465a9ce3c8173771d6cc
Soheil <hesarisoheil1999@example.com>
2025-05-31 10:59:18 +0330

    add a root test

--- apps/spareparts/data/core/spare_parts_db.py
+++ apps/spareparts/data/core/spare_parts_db.py
@@ -1,12 +1,19 @@
-from sqlalchemy.ext.asyncio import create_async_engine , AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 from sqlalchemy.orm import sessionmaker
 
 from apps.spareparts.config import settings
 
-engine = create_async_engine(settings.database_url, echo=True)
 
-AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+def get_engine(url = settings.database_url) -> AsyncEngine :
+    return create_async_engine(url, echo=True)
+
+def get_session(engine = get_engine()) -> sessionmaker:
+    return sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+AsyncSessionLocal = get_session()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
