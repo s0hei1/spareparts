fcaa53c83180f17f413c997fb24f30cbaf5a5431
Soheil <hesarisoheil1999@example.com>
2025-06-11 08:55:30 +0330

    implement an admin interface

---
+++ apps/spareparts/admin/admin.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Depends
+from sqladmin import Admin
+from sqlalchemy.ext.asyncio import AsyncEngine
+from apps.spareparts.admin.admin_models import (
+    PropertyAdmin,
+    SparePartTypeAdmin,
+    SparePartTypePropertiesAdmin,
+    SparePartAdmin,
+    FactoryPartsAdmin,
+    MachineCatalogAdmin,
+    UnitOfMeasureGroupAdmin,
+    UnitOfMeasureAdmin,
+    CompanyAdmin,
+    LocationAdmin,
+    TagAdmin,
+)
+from apps.spareparts.data_layer.core.spare_parts_db import get_engine
+
+
+def config_admin(app: FastAPI, async_engine: AsyncEngine | None = None) -> None:
+    if async_engine is None:
+        async_engine = get_engine()
+
+    admin = Admin(app=app, engine=async_engine)
+
+    admin.add_view(PropertyAdmin)
+    admin.add_view(SparePartTypeAdmin)
+    admin.add_view(SparePartTypePropertiesAdmin)
+    admin.add_view(SparePartAdmin)
+    admin.add_view(FactoryPartsAdmin)
+    admin.add_view(MachineCatalogAdmin)
+    admin.add_view(UnitOfMeasureGroupAdmin)
+    admin.add_view(UnitOfMeasureAdmin)
+    admin.add_view(CompanyAdmin)
+    admin.add_view(LocationAdmin)
+    admin.add_view(TagAdmin)
---
+++ apps/spareparts/admin/admin_models.py
@@ -0,0 +1,59 @@
+from sqladmin import ModelView
+
+from apps.spareparts.data_layer.models.sparepart import (
+    Property,
+    SparePartType,
+    SparePartTypeProperties,
+    SparePart,
+    FactoryParts,
+    MachineCatalog,
+    UnitOfMeasureGroup,
+    UnitOfMeasure,
+    Company,
+    Location,
+    Tag
+)
+
+
+class PropertyAdmin(ModelView, model=Property):
+    column_list = [Property.id, Property.name, Property.value_type, Property.unit_of_measure_id]
+
+
+class SparePartTypeAdmin(ModelView, model=SparePartType):
+    column_list = [SparePartType.id, SparePartType.name, SparePartType.symbol]
+
+
+class SparePartTypePropertiesAdmin(ModelView, model=SparePartTypeProperties):
+    column_list = [SparePartTypeProperties.id, SparePartTypeProperties.spare_part_type_id, SparePartTypeProperties.property_id]
+
+
+class SparePartAdmin(ModelView, model=SparePart):
+    column_list = [SparePart.id, SparePart.name, SparePart.code, SparePart.alias_name, SparePart.spare_part_type_id, SparePart.is_active]
+
+
+class FactoryPartsAdmin(ModelView, model=FactoryParts):
+    column_list = [FactoryParts.id, FactoryParts.name, FactoryParts.parentId, FactoryParts.description]
+
+
+class MachineCatalogAdmin(ModelView, model=MachineCatalog):
+    column_list = [MachineCatalog.id, MachineCatalog.machine_name, MachineCatalog.model_name, MachineCatalog.factory_parts_id, MachineCatalog.is_tool]
+
+
+class UnitOfMeasureGroupAdmin(ModelView, model=UnitOfMeasureGroup):
+    column_list = [UnitOfMeasureGroup.id, UnitOfMeasureGroup.name]
+
+
+class UnitOfMeasureAdmin(ModelView, model=UnitOfMeasure):
+    column_list = [UnitOfMeasure.id, UnitOfMeasure.name, UnitOfMeasure.group_id, UnitOfMeasure.unit_in_group]
+
+
+class CompanyAdmin(ModelView, model=Company):
+    column_list = [Company.id, Company.name, Company.location, Company.website, Company.contactEmail]
+
+
+class LocationAdmin(ModelView, model=Location):
+    column_list = [Location.id, Location.x, Location.y, Location.z, Location.floor]
+
+
+class TagAdmin(ModelView, model=Tag):
+    column_list = [Tag.id, Tag.title]
