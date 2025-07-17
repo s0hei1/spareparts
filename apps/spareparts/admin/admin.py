a64b895877685a2c22eacc45b2f2524063c9baea
Soheil <hesarisoheil1999@example.com>
2025-07-17 09:13:05 +0330

    write tests for machine catalog api

--- apps/spareparts/admin/admin.py
+++ apps/spareparts/admin/admin.py
@@ -12,7 +12,7 @@ from apps.spareparts.admin.admin_models import (
     UnitOfMeasureAdmin,
     CompanyAdmin,
     LocationAdmin,
-    TagAdmin,
+    TagAdmin, TrustDocumentAdmin,
 )
 from apps.spareparts.data_layer.core.spare_parts_db import get_engine
 
@@ -22,15 +22,15 @@ def config_admin(app: FastAPI, async_engine: AsyncEngine | None = None) -> None:
         async_engine = get_engine()
 
     admin = Admin(app=app, engine=async_engine)
-
-    admin.add_view(PropertyAdmin)
-    admin.add_view(SparePartTypeAdmin)
-    admin.add_view(SparePartTypePropertiesAdmin)
-    admin.add_view(SparePartAdmin)
-    admin.add_view(FactoryPartAdmin)
-    admin.add_view(MachineCatalogAdmin)
-    admin.add_view(UnitOfMeasureGroupAdmin)
-    admin.add_view(UnitOfMeasureAdmin)
-    admin.add_view(CompanyAdmin)
-    admin.add_view(LocationAdmin)
-    admin.add_view(TagAdmin)
+    admin.add_view(TrustDocumentAdmin)
+    # admin.add_view(PropertyAdmin)
+    # admin.add_view(SparePartTypeAdmin)
+    # admin.add_view(SparePartTypePropertiesAdmin)
+    # admin.add_view(SparePartAdmin)
+    # admin.add_view(FactoryPartAdmin)
+    # admin.add_view(MachineCatalogAdmin)
+    # admin.add_view(UnitOfMeasureGroupAdmin)
+    # admin.add_view(UnitOfMeasureAdmin)
+    # admin.add_view(CompanyAdmin)
+    # admin.add_view(LocationAdmin)
+    # admin.add_view(TagAdmin)
--- apps/spareparts/admin/admin_models.py
+++ apps/spareparts/admin/admin_models.py
@@ -1,5 +1,6 @@
 from sqladmin import ModelView
 
+from apps.spareparts.business_logic_layer.utils.date_time_helper import gregorian_to_jalali
 from apps.spareparts.data_layer.models.sparepart import (
     Property,
     SparePartType,
@@ -11,12 +12,30 @@ from apps.spareparts.data_layer.models.sparepart import (
     UnitOfMeasure,
     Company,
     Location,
-    Tag
+    Tag, TrustDocument
 )
+
+class TrustDocumentAdmin(ModelView, model=TrustDocument):
+    column_list = [
+        TrustDocument.id,
+        TrustDocument.delivery_date,
+        TrustDocument.return_date,
+        TrustDocument.description,
+        TrustDocument.personal_name,
+    ]
+    name_plural = 'اسناد امانی'
+    column_formatters = {
+        TrustDocument.delivery_date : lambda m, a: gregorian_to_jalali(m.delivery_date),
+        TrustDocument.return_date : lambda m, a: gregorian_to_jalali(m.return_date),
+    }
+
+    column_searchable_list = [TrustDocument.personal_name]
+    column_sortable_list = [TrustDocument.personal_name,]
 
 
 class PropertyAdmin(ModelView, model=Property):
     column_list = [Property.id, Property.name, Property.unit_of_measure_id]
+    name_plural = 'Properties'
 
 
 class SparePartTypeAdmin(ModelView, model=SparePartType):
@@ -24,11 +43,13 @@ class SparePartTypeAdmin(ModelView, model=SparePartType):
 
 
 class SparePartTypePropertiesAdmin(ModelView, model=SparePartTypeProperties):
-    column_list = [SparePartTypeProperties.id, SparePartTypeProperties.spare_part_type_id, SparePartTypeProperties.property_id]
+    column_list = [SparePartTypeProperties.id, SparePartTypeProperties.spare_part_type_id,
+                   SparePartTypeProperties.property_id]
 
 
 class SparePartAdmin(ModelView, model=SparePart):
-    column_list = [SparePart.id, SparePart.name, SparePart.code, SparePart.alias_name, SparePart.spare_part_type_id, SparePart.is_active]
+    column_list = [SparePart.id, SparePart.name, SparePart.code, SparePart.alias_name, SparePart.spare_part_type_id,
+                   SparePart.is_active]
 
 
 class FactoryPartAdmin(ModelView, model=FactoryPart):
@@ -36,7 +57,8 @@ class FactoryPartAdmin(ModelView, model=FactoryPart):
 
 
 class MachineCatalogAdmin(ModelView, model=MachineCatalog):
-    column_list = [MachineCatalog.id, MachineCatalog.machine_name, MachineCatalog.model_name, MachineCatalog.factory_part_id, MachineCatalog.is_tool]
+    column_list = [MachineCatalog.id, MachineCatalog.machine_name, MachineCatalog.model_name,
+                   MachineCatalog.factory_part_id, MachineCatalog.is_tool]
 
 
 class UnitOfMeasureGroupAdmin(ModelView, model=UnitOfMeasureGroup):
@@ -57,3 +79,6 @@ class LocationAdmin(ModelView, model=Location):
 
 class TagAdmin(ModelView, model=Tag):
     column_list = [Tag.id, Tag.title]
+
+
+
