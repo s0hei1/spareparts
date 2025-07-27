2909bb023e9e2e5a6f7e15cfdc7040a00c8c4eed
Soheil <hesarisoheil1999@example.com>
2025-07-27 13:53:20 +0330

    batch insert for uom groups

--- apps/spareparts/api_layer/api/uint_of_measue_api.py
+++ apps/spareparts/api_layer/api/uint_of_measue_api.py
@@ -10,7 +10,8 @@ from apps.spareparts.data_layer.repository.unit_of_measure_repository import Uni
 from apps.spareparts.di.bll_dependencies import BLL_DI
 from apps.spareparts.di.repository_dependencies import RepositoryDI
 
-uom_router = APIRouter(prefix ='/uom', tags=['Unit of Measure'])
+uom_router = APIRouter(prefix='/uom', tags=['Unit of Measure'])
+
 
 @uom_router.get('/read_many', response_model=list[UnitOfMeasureRead])
 async def read_many(
@@ -19,32 +20,37 @@ async def read_many(
     objs = await uom_repo.read_many()
     return UnitOfMeasureRead.from_unit_of_measures(objs)
 
+
 @uom_router.get('/read_one', response_model=UnitOfMeasureRead)
 async def read_one(
-        id : int,
+        id: int,
         uom_repo: UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
 ):
-    obj= await uom_repo.read_one(id=id)
+    obj = await uom_repo.read_one(id=id)
     return UnitOfMeasureRead.from_unit_of_measure(obj)
 
-@uom_router.post('/add', response_model= UnitOfMeasureRead )
-async def add_uom(uom_create : UnitOfMeasureCreate,
-                  uom_repo : UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
+
+@uom_router.post('/add', response_model=UnitOfMeasureRead)
+async def add_uom(uom_create: UnitOfMeasureCreate,
+                  uom_repo: UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
                   ):
-    obj =  await uom_repo.create(uom_create.to_unit_of_measure())
+    obj = await uom_repo.create(uom_create.to_unit_of_measure())
     return UnitOfMeasureRead.from_unit_of_measure(obj)
 
-@uom_router.put('/update', response_model= UnitOfMeasureRead )
-async def update_uom(uom_update : UnitOfMeasureUpdate,
-                  uom_repo : UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
-                  ):
+
+@uom_router.put('/update', response_model=UnitOfMeasureRead)
+async def update_uom(uom_update: UnitOfMeasureUpdate,
+                     uom_repo: UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
+                     ):
     return await uom_repo.update(**uom_update.model_dump())
 
-@uom_router.delete('/delete', response_model= UnitOfMeasureRead )
+
+@uom_router.delete('/delete', response_model=UnitOfMeasureRead)
 async def delete_uom(id: int,
-                  uom_repo : UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
-                  ):
-    return await uom_repo.delete(id= id)
+                     uom_repo: UnitOfMeasureRepository = Depends(RepositoryDI.unit_of_measure_repository)
+                     ):
+    return await uom_repo.delete(id=id)
+
 
 @uom_router.get('/read_many_groups', response_model=list[UnitOfMeasureGroupRead])
 async def read_many_groups(
@@ -52,13 +58,25 @@ async def read_many_groups(
 ):
     return await uom_group_repo.read_many()
 
-@uom_router.post('/add_group', response_model= UnitOfMeasureGroupRead )
-async def add_uom_group(uom_create : UnitOfMeasureGroupCreate,
-                  uom_group_repo: UnitOfMeasureGroupRepository = Depends(RepositoryDI.unit_of_measure_group_repository)
-                  ):
+
+@uom_router.post('/add_group', response_model=UnitOfMeasureGroupRead)
+async def add_uom_group(uom_create: UnitOfMeasureGroupCreate,
+                        uom_group_repo: UnitOfMeasureGroupRepository = Depends(
+                            RepositoryDI.unit_of_measure_group_repository)
+                        ):
     return await uom_group_repo.create(uom_create.to_unit_of_measure_group())
 
-@uom_router.delete('/delete_group', response_model= UnitOfMeasureGroupDelete )
+
+@uom_router.post('/create_many', response_model=list[UnitOfMeasureGroupRead])
+async def create_many(uom_create: list[UnitOfMeasureGroupCreate],
+                      uom_group_repo: UnitOfMeasureGroupRepository = Depends(
+                             RepositoryDI.unit_of_measure_group_repository)
+                      ):
+
+    return await uom_group_repo.create_many([i.to_unit_of_measure_group() for i in uom_create])
+
+
+@uom_router.delete('/delete_group', response_model=UnitOfMeasureGroupDelete)
 async def delete_uom_group(id: int,
                            uom_group_repo: UnitOfMeasureGroupRepository = Depends(
                                RepositoryDI.unit_of_measure_group_repository)
---
+++ tests/spareparts_tests/api_layer/uom_api_tests.py
@@ -0,0 +1,35 @@
+import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from global_fixtures import app, async_client
+
+@pytest.fixture()
+def fake_uom_groups():
+    return [
+        {
+            "name": "Test Group 1",
+            "alias_name": "Test Group Alias Name",
+            "description": "this is a test",
+        },
+        {
+            "name": "Test Group 2",
+            "alias_name": "Test Group Alias Name",
+            "description": "this is a test",
+        },
+        {
+            "name": "Test Group 3",
+            "alias_name": "Test Group Alias Name",
+            "description": "this is a test",
+        },
+    ]
+
+@pytest.mark.asyncio
+async def test_create_uom_groups(
+        app : FastAPI,
+        async_client : AsyncClient,
+        fake_uom_groups):
+    response = await async_client.post('/uom/create_many', json=fake_uom_groups)
+    response.raise_for_status()
+    assert isinstance(response.json(), list)
+    assert len(response.json()) == len(fake_uom_groups)
+
